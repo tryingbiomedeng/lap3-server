0ce61be2d4b8eb69e872e9115ed09d79a436dfa8
Cams-Plan <ca.francis@outlook.com>
2023-09-11 17:37:11 +0100

    basic api test passed

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -1,1 +1,29 @@
 require("dotenv").config({path: './test/.env.test'})
+
+//basic api imports
+const request = require("supertest");
+const server = require("../app");
+
+// TODO: Auth Imports
+
+// TODO Middleware Imports
+
+// TODO Security Imports
+
+describe("API tests", () => {
+    let app;
+
+    beforeAll(() => {
+        app = server.listen(process.env.TEST_PORT);
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        app.close();
+    });
+
+    it("should return a 200 status code for GET request to /", async () => {
+        const response = await request(app).get("/");
+        expect(response.statusCode).toBe(200)
+    }) 
+})
