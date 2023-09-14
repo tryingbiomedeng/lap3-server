f9358bbb7bbc722f868c85a3c11792d94d29eda0
Cams-Plan <ca.francis@outlook.com>
2023-09-14 21:47:45 +0100

    notes tests redone

--- __tests__/controllers/notesControllers.spec.js
+++ __tests__/controllers/notesControllers.spec.js
@@ -33,29 +33,29 @@ describe("Controllers tests", () => {
                 _id: "ObjectId('6500787545ec1cab08690bf8')",
                 username: 'user3',
                 title: 'PE Training Type',
-                subject: 'Sciene',
+                subject: 'Science',
                 topic_tags: [ 'training' ],
                 content: 'yes, PE still needs notes',
                 createdAt: "2023-09-12T14:40:08.461Z",
-                updatedAt: Date.now(),
+                updatedAt: "2023-09-12T14:40:08.461Z",
                 __v: 0
             },
             full2: {
                 "_id": "6500785845ec1cab08690bf4",
-                "username": "username2",
-                "title": "Title2",
-                "subject": "Subject2",
-                "topic_tags": ["tag2"],
-                "content": "content2",
+                "username": "user3",
+                "title": "PE Training Type",
+                "subject": "Science",
+                "topic_tags": ['training'],
+                "content": "'yes, PE still needs notes'",
                 "createdAt": "2023-09-12T14:40:08.461Z",
                 "updatedAt": "2023-09-12T14:40:08.461Z",
                 "__v": 0
             },
             noDC: {
-                _id: "ObjectId('6500787545ec1cab08690bf8')",
+                _id: '6500787545ec1cab08690bf8',
                 username: 'user3',
                 title: 'PE Training Type',
-                subject: 'Sciene',
+                subject: 'Science',
                 topic_tags: [ 'training' ],
                 content: 'yes, PE still needs notes',
 
@@ -176,23 +176,25 @@ describe("Controllers tests", () => {
 
         test("Should return trigger the Note.find() within the notesControllers.js", async () => { 
 
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+
             const findQuery = jest.spyOn(Note, 'find')
                 .mockResolvedValueOnce(sampleData[0].full2)
 
             const response = await request(app).get(`/notes/tag/${sampleData[0].full2.topic_tags}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
-
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
+            expect(usernameFindQuery).toHaveBeenCalledTimes(1)
             expect(findQuery).toHaveBeenCalledTimes(1);
             
         }),
 
         test("Should return Status 200 and correct response for sucessful requests", async () => {
 
-            const findQuery = jest.spyOn(Note, 'find')
-                .mockResolvedValueOnce(sampleData[0].full2)
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            const findQuery = jest.spyOn(Note, 'find').mockResolvedValueOnce(sampleData[0].full2)
 
             const response = await request(app).get(`/notes/tag/${sampleData[0].full2.topic_tags}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
 
             expect(response.statusCode).toBe(200);
             expect(response.body).toEqual({success: true, response: sampleData[0].full2})
@@ -200,11 +202,11 @@ describe("Controllers tests", () => {
 
         test("Should return Status 404 and correct response for unsuccessful requests", async () => {
 
-            const findQuery = jest.spyOn(Note, 'find')
-                .mockRejectedValueOnce("error")
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            const findQuery = jest.spyOn(Note, 'find').mockRejectedValueOnce("error")
 
             const response = await request(app).get(`/notes/tag/${sampleData[0].full2.topic_tags}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
 
             expect(findQuery).toHaveBeenCalledTimes(1)
             expect(response.statusCode).toBe(404);
@@ -213,44 +215,69 @@ describe("Controllers tests", () => {
     })
 
     describe('POST /notes TESTS', () => { 
+        let newNote = new Note();
 
-        test("Should return trigger the Note.create() within the notesControllers.js", async () => { 
+        test("Should trigger Token.find() within the notesControllers.js", async () => { 
 
-            const findQuery = jest.spyOn(Note, 'create')
-                .mockResolvedValueOnce(sampleData[0].full2)
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            
+            const noteInstance = jest.spyOn(newNote, "save").mockResolvedValueOnce(sampleData[0].full2)
 
             const response = await request(app).post(`/notes`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].full2})
 
-            expect(findQuery).toHaveBeenCalledTimes(1);      
+            expect(usernameFindQuery).toHaveBeenCalledTimes(1)     
+        }),
+
+        test("Should trigger a new instance of Note within the notesControllers.js", async () => { 
+
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            let newNote = new Note();
+            const noteInstance = jest.spyOn(newNote, "save").mockResolvedValueOnce(sampleData[0].full2)
+
+            const response = await request(app).post(`/notes`)
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
+                .send({body: sampleData[0].full2})
+
+            expect(noteInstance).toHaveBeenCalledTimes(1);
         }),
 
         test("Should return Status 201 and correct response for sucessful requests", async () => {
-
-            const findQuery = jest.spyOn(Note, 'create')
-                .mockResolvedValueOnce(sampleData[0].full1)
-
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            
+            const noteInstance = jest.spyOn(newNote, "save").mockResolvedValueOnce(sampleData[0].full2)
             const response = await request(app).post(`/notes`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
-                .send({body: sampleData[0].noIdDates1})
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
+                .send({body: sampleData[0].noIdDates1}) 
 
-            expect(response.statusCode).toBe(201);
-            expect(response.body).toEqual({success: true, response: sampleData[0].full1})
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual({success: true, response: sampleData[0].full2})
         }),
 
         test("Should return Status 404 and correct response for unsuccessful requests", async () => {
 
-            const findQuery = jest.spyOn(Note, 'create')
-                .mockRejectedValueOnce("error")
-
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            const noteInstanceSave = jest.spyOn(newNote, "save").mockRejectedValueOnce("undefined")
             const response = await request(app).post(`/notes`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
+                .send({body: sampleData[0].noIdDates1})
+            
+            expect(noteInstanceSave).toHaveBeenCalledTimes(1)
+            expect(response.statusCode).toBe(404);
+            expect(response.body).toEqual({success: false, message: "Unable to create new note", "error": "undefined"})
+        })
+
+        test("Should return Status 404 and correct response for invalid note", async () => {
+            const usernameFindQuery = jest.spyOn(Token, "find").mockResolvedValueOnce(sampleData[0].full2.username)
+            const noteInstanceSave = jest.spyOn(newNote, "save").mockRejectedValueOnce("undefined")
+            const response = await request(app).post(`/notes`)
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].noIdDates1})
 
-            expect(findQuery).toHaveBeenCalledTimes(1)
+            expect(noteInstanceSave).toHaveBeenCalledTimes(1)
             expect(response.statusCode).toBe(404);
-            expect(response.body).toEqual({success: false, message: "Unable to create new note", "error": "error"})
+            expect(response.body).toEqual({success: false, message: "Unable to create new note", error: "undefined"})
         })
     }),
 
@@ -262,7 +289,7 @@ describe("Controllers tests", () => {
                 .mockResolvedValueOnce(sampleData[0].full1)
 
             const response = await request(app).patch(`/notes/${sampleData[0].noDC._id}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].noDC})
 
             expect(findQuery).toHaveBeenCalledTimes(1);        
@@ -274,7 +301,7 @@ describe("Controllers tests", () => {
                 .mockResolvedValueOnce(sampleData[0].full1)
 
             const response = await request(app).patch(`/notes/${sampleData[0].noDC._id}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].noDC})
 
             expect(response.statusCode).toBe(200);
@@ -287,7 +314,7 @@ describe("Controllers tests", () => {
                 .mockRejectedValueOnce("error")
 
             const response = await request(app).patch(`/notes/${sampleData[0].noDC._id}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].noDC})
 
             expect(findQuery).toHaveBeenCalledTimes(1)
@@ -304,7 +331,7 @@ describe("Controllers tests", () => {
                 .mockResolvedValueOnce(sampleData[0].full1)
 
             const response = await request(app).delete(`/notes/${sampleData[0].noDC._id}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
 
             expect(findQuery).toHaveBeenCalledTimes(1);         
         }),
@@ -315,7 +342,7 @@ describe("Controllers tests", () => {
                 .mockResolvedValueOnce(sampleData[0].full1)
 
             const response = await request(app).delete(`/notes/${sampleData[0].noDC._id}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].noDC})
 
             expect(response.statusCode).toBe(204);
@@ -324,15 +351,15 @@ describe("Controllers tests", () => {
         test("Should return Status 404 and correct response for unsuccessful requests", async () => {
 
             const findQuery = jest.spyOn(Note, 'findByIdAndDelete')
-                .mockRejectedValueOnce("error")
+                .mockRejectedValueOnce("undefined")
 
             const response = await request(app).delete(`/notes/${sampleData[0].noDC._id}`)
-                .set({'Accept': 'application/json', 'Authorization': 'tokenValue' })
+                .set({'Accept': 'application/json', 'Authorization': sampleData[1].token1 })
                 .send({body: sampleData[0].noDC})
 
             expect(findQuery).toHaveBeenCalledTimes(1)
             expect(response.statusCode).toBe(404);
-            expect(response.body).toEqual({success: false, message: "Unable to delete note", "error": "error"})
+            expect(response.body).toEqual({success: false, message: "Unable to delete note." + " undefined", "error": "undefined"})
         })
     })
 })
